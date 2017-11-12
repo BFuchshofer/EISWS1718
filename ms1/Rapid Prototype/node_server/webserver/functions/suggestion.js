69ff4a98d1cd34f32ca89e5717a1a8a57841808a
eXeLuis <exe.niklas@googlemail.com>
2017-11-12 14:35:44 +0100

    FIXED suggestion.js - fehler behoben durch den der letzte raum anstatt der 'beste' raum ausgegeben wurde

--- ms1/Rapid Prototype/node_server/webserver/functions/suggestion.js
+++ ms1/Rapid Prototype/node_server/webserver/functions/suggestion.js
@@ -23,6 +23,8 @@ function getVeranstaltungen( callback ){
                 callback( veranstaltungen );
 
             });
+        } else if( externalResponse.statusCode == 204 ){
+            callback( null );
         }
     });
     externalRequest.end();
@@ -49,6 +51,8 @@ function getEmptyRooms( callback ){
                 callback( rooms );
 
             });
+        } else if( externalResponse.statusCode == 204 ){
+            callback( null );
         }
     });
     externalRequest.end();
@@ -68,52 +72,83 @@ function getSuggestion( filter, callback ){
     ////////////////////////
     getVeranstaltungen( function( result ){
         getEmptyRooms( function( rooms ){
-            for( var i = 0; i < rooms.length; i++ ){
-                //console.log( 'Loop: ' + i );
-                //console.log( rooms[i].number );
-                for( var j = 0; j < result.length; j++ ){
-                    //console.log( '- ' +  result[j].room );
-                    // Bricht den Schleifendurchlauf ab, wenn die momentane Zeit innerhalb einer Veranstaltungszeit liegt UND die betreffenden Räume die selben sind
-                    if(( result[j].begin<=(hour*100+min) && result[j].end>=(hour*100+min) ) && rooms[i].number == result[j].room ){
-                        //console.log( 'rip' );
-                        break;
+            if( rooms != null ){
+                for( var i = 0; i < rooms.length; i++ ){
+
+
+                    //console.log( ' ' );
+                    //console.log( '==================================' );
+                    //console.log( 'Loop:        ' + i );
+                    //console.log( '==================================' );
+                    //console.log( 'SizeMAX:     ' + size_max );
+                    //console.log( 'RoomNR:      ' + rooms[i].number );
+
+
+                    for( var j = 0; j < result.length; j++ ){
+
+                        console.log( '             - ' +  result[j].room );
+
+                        // Bricht den Schleifendurchlauf ab, wenn die momentane Zeit innerhalb einer Veranstaltungszeit liegt UND die betreffenden Räume die selben sind
+                        if(( result[j].begin<=(hour*100+min) && result[j].end>=(hour*100+min) ) && rooms[i].number == result[j].room ){
+
+                            console.log( 'RIP' );
+
+                            break;
+                        }
+                        // Bricht den Scheifendurchlauf ab, wenn der betreffende Raum innerhalb der nächsten Stunde( Belegungszeit ) durch eine Veranstaltung belegt ist.
+                        if( result[j].begin<=((hour+1)*100+min) && result[j].end>=((hour+1)*100+min) && rooms[i].number == result[j].room ){
+
+                            console.log( 'RIP2' );
+
+                            break;
+                        }
+
+
+
+
+
+
                     }
-                    // Bricht den Scheifendurchlauf ab, wenn der betreffende Raum innerhalb der nächsten Stunde( Belegungszeit ) durch eine Veranstaltung belegt ist.
-                    if( result[j].begin<=((hour+1)*100+min) && result[j].end>=((hour+1)*100+min) && rooms[i].number == result[j].room ){
-                        //console.log( 'rip2' );
-                        break;
-                    }
+                    if( filter == null ){
+                        // Bricht den Schleifendurchlauf ab, wenn die maximale Größe des Raumes ( wie viele Sitzplätze maximal ) größer ist als vom bisher kleinsten raum
 
+                        //console.log( '----------------------------------' );
+                        //console.log( 'SizeMAX:     ' + size_max );
+                        //console.log( 'RoomSizeMAX: ' + rooms[i].size_max );
 
+                        if( parseInt( rooms[i].size_max, 10 ) > size_max && size_max != 0 ){
 
+                            //console.log( '----------------------------------' );
+                            //console.log( '             ' + rooms[i].number );
+                            //console.log( '----------------------------------' );
 
-                }
-                if( filter == null ){
-                    // Bricht den Schleifendurchlauf ab, wenn die maximale Größe des Raumes ( wie viele Sitzplätze maximal ) größer ist als vom bisher kleinsten raum
-                    if( rooms[i].size_max > size_max && size_max != 0 ){
-                        //console.log( rooms[i].size_max );
-                        //console.log( size_max );
-                        //console.log( rooms[i].number + ':/' );
+                        } else {
+                            //console.log( '----------------------------------' );
+                            //console.log( 'Best Room:   ' + rooms[i].number );
+                            //console.log( '----------------------------------' );
+                            size_max = rooms[i].size_max;
+
+                            suggestion = rooms[i].number;
+                        }
                     } else {
-                        //console.log( rooms[i].number + ' Keepo' );
-                        //console.log( rooms[i].size_max );
-                        size_max = rooms[i].size_max;
+                        //TODO: Schließe Räume anhand des 'filter' aus
                     }
-                } else {
-                    //TODO: Schließe Räume anhand des 'filter' aus
+
+                    //TODO: Gewichtung nach Equipment
+
+                    if( i == (rooms.length - 1 )){
+
+                        //console.log( 'Suggestion:  ' + suggestion );
+                        //callback( suggestion );
+
+                    }
+
                 }
-                suggestion = rooms[i].number;
-
-                //TODO: Gewichtung nach Equipment
-
-                if( i == (rooms.length - 1 )){
-
-                    //console.log( 'Suggestion: ' + suggestion );
-                    //callback( suggestion );
-                }
-
+                callback( suggestion );
+            } else {
+                //console.log( 'NO ROOMS' );
+                callback( null );
             }
-            callback( suggestion );
         });
     });
 }

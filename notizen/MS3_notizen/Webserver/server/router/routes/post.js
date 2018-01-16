6e5cea825d4696a31ad9ba7b2da1861e50e59a20
Bastian Fuchshofer <bastian.f.1993@web.de>
2018-01-16 18:39:09 +0100

    UPDATED singleRoom Search Requests

--- notizen/MS3_notizen/Webserver/server/router/routes/post.js
+++ notizen/MS3_notizen/Webserver/server/router/routes/post.js
@@ -1,32 +1,53 @@
 var router                  = express.Router();
 
-
+// Raum anfragen und ggf. updaten
 router.post( '/room', function( req, res ){
     req.on( 'data', function( chunk ){
         var data                = JSON.parse( chunk );
         
-        if (data.token != null) {
+        var newData = {
+                "data":{
+                    "beacon":data.beacon,
+                    "person":data.person,
+                    "roomSize":data.roomSize,
+                    "blackboard":data.blackboard,
+                    "whiteboard":data.whiteboard,
+                    "beamer":data.beamer  
+               }
+            };
+        
+        if (data.token == "GET") {
+            
+            // TODO
+            // neue Raumabfrage
+            
+            console.log("Get new Room");
+            res.status(200).send( data ); 
+            
+        } else if (data.token == "UPDATE") {
+            
+            // TODO
+            // neue Raumabfrage, im prinzip wie bei GET?
+            
             console.log( "Update Room" );
             res.status(200).send( data ); 
+            
+        } else if (data.token == "BOOK") {
+            
+            
+            // Raum buchen
+            
+            console.log("Book Room");
+            res.status(200).send( data ); 
         } else {
-            var newData = {
-            "data":{
-                "whiteboard":data.whiteboard,
-                "person":data.person,
-                "beacon":data.beacon,
-                "beamer":data.beamer,
-                "blackboard":data.blackboard,
-                "roomSize":data.roomSize,
-                "token": 1234567890
-            }
-            };
-    
-            console.log( "Get New Room" );
-            res.status(200).send( JSON.stringify( newData ) ); 
+            console.log("Error");
+            res.status(400).send( data ); 
         }
-    
     });
 });
+
+  
+
 
 
 

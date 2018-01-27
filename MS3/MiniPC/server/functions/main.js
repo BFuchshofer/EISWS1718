4b11d5387567f7a3923dd468013494e7491e1b87
Bastian Fuchshofer <bastian.f.1993@web.de>
2018-01-27 17:08:51 +0100

    update

--- MS3/MiniPC/server/functions/main.js
+++ MS3/MiniPC/server/functions/main.js
@@ -8,89 +8,80 @@ var testString = "id"; // abändern in irgendeinen Key der im Array curentData (
 
 // Liefert die Liste der aktuellen Rauminhalte für den Request
 function getList() {
-var data = readFile();
-return data;
+    var data = readFile();
+    return data;
 }
 
 // Liest die Datei aus in der sich die dokumentierten aktuellen Rauminhalte befinden
 function readFile() {
-var data;
-data = fs.readFileSync(curentRoom, 'utf8');
-if (data != "") {
-	return JSON.parse(data);
-} else {
-
-return data;
-}
+    var data;
+    data = fs.readFileSync(curentRoom, 'utf8');
+    if (data != "") {
+	   return JSON.parse(data);
+    } else {
+        return data;
+    }
 }
 
 
 // Schreibt Änderungen der Rauminhalte in die Datei fest
 function writeFile(data) {
-var testdata = data
-var curentData = readFile(curentRoom);
-var tmp = {"data":[]};
+    var testdata = data
+    var curentData = readFile(curentRoom);
+    var tmp = {"data":[]};
 
-if (curentData.data.length > 0) {
-curentData.data.push(testdata);
-fs.writeFileSync(curentRoom, JSON.stringify(curentData));
-console.log('Data saved: ' + JSON.stringify(curentData));
-} else {
-tmp.data.push(testdata);
-fs.writeFileSync(curentRoom, JSON.stringify(tmp));
-console.log('Data saved: ' + JSON.stringify(tmp));
-}
+    if (curentData.data.length > 0) {
+        curentData.data.push(testdata);
+        fs.writeFileSync(curentRoom, JSON.stringify(curentData));
+        console.log('Data saved: ' + JSON.stringify(curentData));
+    } else {
+        tmp.data.push(testdata);
+        fs.writeFileSync(curentRoom, JSON.stringify(tmp));
+        console.log('Data saved: ' + JSON.stringify(tmp));
+    }
 }
 
 
 // Reagiert auf Meldungen durch den RFID Scanner (simuliert) und aktualisiert die Liste mit Rauminhalten
 function updateList(item) {
 
-var curentData = readFile();
-item = JSON.parse(item);
-if (curentData.data.length == 0) {
-var time = Date.now();
-item.timestamp = time
-curentData.data.push(item);
-fs.writeFileSync(curentRoom, JSON.stringify(curentData));
-console.log("Added First: " + JSON.stringify(item));
-
-sendList();
-
-} else if (curentData.data.length <= 19) {
-var check = false;
-for (var i = 0; i < curentData.data.length; i++) {
-if (parseInt(item.id) == parseInt(JSON.stringify(curentData.data[i].id))) {
-check = true;
-curentData.data.splice(i,1);
-fs.writeFileSync(curentRoom, JSON.stringify(curentData));
-console.log("Removed: " + JSON.stringify(item));
-
-sendList();
-
-} // if
-} // for
-if (check === false) {
-var time = Date.now();
-item.timestamp = time
-curentData.data.push(item);
-fs.writeFileSync(curentRoom, JSON.stringify(curentData));
-console.log("Added: " + JSON.stringify(item));
-check = true;
-sendList();
-
-}  // if
-} // else if
-
+    var curentData = readFile();
+    item = JSON.parse(item);
+    if (curentData.data.length == 0) {
+        var time = Date.now();
+        item.timestamp = time
+        curentData.data.push(item);
+        fs.writeFileSync(curentRoom, JSON.stringify(curentData));
+        console.log("Added First: " + JSON.stringify(item));
+        sendList();
+    } else if (curentData.data.length <= 19) {
+        var check = false;
+        for (var i = 0; i < curentData.data.length; i++) {
+            if (parseInt(item.id) == parseInt(JSON.stringify(curentData.data[i].id))) {
+                check = true;
+                curentData.data.splice(i,1);
+                fs.writeFileSync(curentRoom, JSON.stringify(curentData));
+                console.log("Removed: " + JSON.stringify(item));
+                sendList();
+            } // if
+        } // for
+        if (check === false) {
+            var time = Date.now();
+            item.timestamp = time
+            curentData.data.push(item);
+            fs.writeFileSync(curentRoom, JSON.stringify(curentData));
+            console.log("Added: " + JSON.stringify(item));
+            check = true;
+            sendList();
+        }  // if
+    } // else if
 } // function
 
 
 // Sendet sobald eine aktualisierung vorliegt, die Liste mit den Rauminhalten an den Server
 function sendList() {
-var data = readFile();
-data = JSON.stringify(data);
-
-
+    var data = readFile();
+    data = JSON.stringify(data);
     var options = {
         host: VARIABLES.webserver.address,
         port: VARIABLES.webserver.port,
@@ -101,23 +92,20 @@ data = JSON.stringify(data);
 	    'Content-Length': Buffer.byteLength(data)
         }
     }
-
     var exReq = http.request(options, function(exRes){
-            exRes.on("data", function(chunk){
-		if (exRes.statusCode == 200) {
-			console.log('POST /roomList - OK');
-			}
-            });
-
+        exRes.on("data", function(chunk){
+            if (exRes.statusCode == 200) {
+                console.log('POST /roomList - OK'); 
+            }
+        });
     });
-exReq.write(data);
+    exReq.write(data);
     exReq.end();
-
 }
 
 // Sendet die IP des Minipcs bei Serverstart an den Server
 function sendIP() {
-var options = {
+    var options = {
         host: VARIABLES.webserver.address,
         port: VARIABLES.webserver.port,
         path: '/miniPcPing',
@@ -127,34 +115,30 @@ var options = {
         }
     }
 
-var data = {
-	room_id: VARIABLES.room.room_id,
-	host: IP.address(),
-	port: VARIABLES.minipc.port
+    var data = {
+	   room_id: VARIABLES.room.room_id,
+	   host: IP.address(),
+	   port: VARIABLES.minipc.port
 	}
 
-var statusCode;
-
-
-
+    var statusCode;
     var exReq = http.request(options, function(exRes){
-       
-exRes.on("data", function(chunk){
-
-		if (exRes.statusCode != 200) {
-            		sendIP();
-			console.log("IP Not Send");
-			//exRes.end();
-		} else {
-			console.log("IP Send");
-			//exRes.end();
-		} 
-            });	
+        exRes.on("data", function(chunk){
+            if (exRes.statusCode != 200) {
+                sendIP();
+                console.log("IP Not Send");
+                //exRes.end();
+            } else {
+                console.log("IP Send");
+                //exRes.end();
+            } 
+        });	
     });
-exReq.on("error", function(err) {
-		console.log("Error in Request" + err);
-	}); 
-	exReq.write(JSON.stringify(data));
+    
+    exReq.on("error", function(err) {
+        console.log("Error in Request" + err);
+    }); 
+    exReq.write(JSON.stringify(data));
     exReq.end();
 }
 
